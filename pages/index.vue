ad84a920069b62db4246737d244b544d0e28a054
seiwaldm <seiwald.markus@hak-stjo.salzburg.at>
2022-12-10 19:38:17 +0100

    Update index.vue

--- pages/index.vue
+++ pages/index.vue
@@ -8,11 +8,17 @@ definePageMeta({ alias: ["/"] });
   <ion-page>
     <ion-header>
       <ion-toolbar pr-4>
-        <ion-title>Home</ion-title>
-        <div slot="end" flex gap-2>
-          <ion-icon :icon="menu" />
+        <div flex gap-4>
+          <ion-title>Home</ion-title>
+          <ion-text>different Icons:</ion-text>
+
           <Icon icon="ion:logo-ionic" />
           <Icon icon="logos:ionic" />
+          <Icon icon="ion:menu" />
+          <ion-icon id="click-trigger" :icon="menu" />
+          <ion-popover trigger="click-trigger" trigger-action="click">
+            <ion-content class="ion-padding">Hello World!</ion-content>
+          </ion-popover>
         </div>
       </ion-toolbar>
     </ion-header>
