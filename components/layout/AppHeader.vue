658046471f0b1e72b21fbd433fabe9bc1773118a
seiwaldm <seiwald.markus@hak-stjo.salzburg.at>
2022-12-10 22:40:38 +0100

    Update AppHeader.vue

--- components/layout/AppHeader.vue
+++ components/layout/AppHeader.vue
@@ -7,9 +7,10 @@ import { Icon } from "@iconify/vue";
     <ion-toolbar pr-4>
       <div flex gap-4>
         <ion-title>Home</ion-title>
+
         <Icon icon="logos:nuxt" />
         <Icon icon="logos:ionic" />
-        <Icon id="click-trigger" icon="ion:menu" />
+        <Icon id="click-trigger" icon="ion:menu" cursor-pointer />
         <ion-popover trigger="click-trigger" trigger-action="click">
           <ion-content class="ion-padding">Hello World!</ion-content>
         </ion-popover>
