7cd259d79de4636e76d4a26b16d650378443e9f7
seiwaldm <seiwald.markus@hak-stjo.salzburg.at>
2022-12-10 22:03:26 +0100

    built header component

---
+++ components/layout/AppHeader.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+import { Icon } from "@iconify/vue";
+</script>
+
+<template>
+  <ion-header>
+    <ion-toolbar pr-4>
+      <div flex gap-4>
+        <ion-title>Home</ion-title>
+        <Icon icon="logos:nuxt" />
+        <Icon icon="logos:ionic" />
+        <Icon id="click-trigger" icon="ion:menu" />
+        <ion-popover trigger="click-trigger" trigger-action="click">
+          <ion-content class="ion-padding">Hello World!</ion-content>
+        </ion-popover>
+      </div>
+    </ion-toolbar>
+  </ion-header>
+</template>
--- pages/index.vue
+++ pages/index.vue
@@ -1,27 +1,10 @@
 <script setup lang="ts">
-import { menu } from "ionicons/icons";
-import { Icon } from "@iconify/vue";
 definePageMeta({ alias: ["/"] });
 </script>
 
 <template>
   <ion-page>
-    <ion-header>
-      <ion-toolbar pr-4>
-        <div flex gap-4>
-          <ion-title>Home</ion-title>
-          <ion-text>different Icons:</ion-text>
-
-          <Icon icon="ion:logo-ionic" />
-          <Icon icon="logos:ionic" />
-          <Icon icon="ion:menu" />
-          <ion-icon id="click-trigger" :icon="menu" />
-          <ion-popover trigger="click-trigger" trigger-action="click">
-            <ion-content class="ion-padding">Hello World!</ion-content>
-          </ion-popover>
-        </div>
-      </ion-toolbar>
-    </ion-header>
+    <layout-app-header />
     <ion-content class="ion-padding">
       <h1>Home</h1>
       <ion-button>Default</ion-button>
