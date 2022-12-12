88713c98ee3f87a8f3a8b0deb8f5a88f0e828ea5
seiwaldm <seiwald.markus@hak-stjo.salzburg.at>
2022-12-12 21:08:07 +0100

    added routing

--- components/layout/AppHeader.vue
+++ components/layout/AppHeader.vue
@@ -1,18 +1,33 @@
 <script setup lang="ts">
 import { Icon } from "@iconify/vue";
+const router = useIonRouter();
+
+const showMenu = ref(false);
 </script>
 
 <template>
   <ion-header>
     <ion-toolbar pr-4>
       <div flex gap-4>
-        <ion-title>Home</ion-title>
+        <ion-title router-link="/" cursor-pointer>Go Home</ion-title>
 
         <Icon icon="logos:nuxt" />
         <Icon icon="logos:ionic" />
-        <Icon id="click-trigger" icon="ion:menu" cursor-pointer />
-        <ion-popover trigger="click-trigger" trigger-action="click">
-          <ion-content class="ion-padding">Hello World!</ion-content>
+
+        <Icon
+          @click="showMenu = true"
+          id="menuButton"
+          icon="ion:menu"
+          cursor-pointer
+        />
+        <ion-popover
+          trigger="menuButton"
+          @didDismiss="showMenu = false"
+          :is-open="showMenu"
+        >
+          <ion-content class="ion-padding">
+            <ion-button @click="router.push('/test')">test</ion-button>
+          </ion-content>
         </ion-popover>
       </div>
     </ion-toolbar>
---
+++ pages/test/index.vue
@@ -0,0 +1,8 @@
+<template>
+  <IonPage>
+    <LayoutAppHeader />
+    <IonContent class="ion-padding">
+      <div>Test passed!</div>
+    </IonContent></IonPage
+  >
+</template>
